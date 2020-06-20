f3e1445bdfddbd13afd205f4dabc748f415d4fc0
lathisha <lathisha.n@gmail.com>
2020-06-19 23:00:47 -0700

    sql database created

---
+++ employeeSchema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR (30),
+    -- salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
