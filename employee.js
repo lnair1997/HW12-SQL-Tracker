d6307252a0d16d89d8aa4b0c039ef637b2926d04
lathisha <lathisha.n@gmail.com>
2020-06-19 21:20:19 -0700

    inital connection

--- employee.js
+++ employee.js
@@ -1,3 +1,20 @@
 const mysql = require("mysql2/promise");
 const inquirer = require("inquirer");
 const cTbl = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+ 
+    port: 3306,
+
+    user: "root",
+  
+    password: "",
+    database: ""
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+
+    start();
+  });
