7de94c64819872e3a3d4735a69fd7e5f39860335
lathisha <lathisha.n@gmail.com>
2020-06-22 13:32:06 -0700

    npm installs

--- employee.js
+++ employee.js
@@ -1,16 +1,13 @@
 const mysql = require("mysql2/promise");
 const inquirer = require("inquirer");
-const cTbl = require("console.table");
+// const cTbl = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
- 
     port: 3306,
-
     user: "root",
-  
     password: "",
-    database: ""
+    database: "employee_db"
   });
   
   connection.connect(function(err) {
